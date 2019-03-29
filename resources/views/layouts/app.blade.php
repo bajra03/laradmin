94ac68b819483857c251d117d715c39c557c9b44
bajra03 <putu.bajra@gmail.com>
2019-03-29 12:13:03 +0800

    initial commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+
+@section('htmlheader')
+    @include('layouts.partials.htmlheader')
+@show
+
+
+<body id="page-top">
+
+  <!-- Page Wrapper -->
+  <div id="wrapper">
+
+    <!-- Sidebar -->
+    @include('layouts.partials.sidebar')
+    <!-- End of Sidebar -->
+
+    <!-- Content Wrapper -->
+    <div id="content-wrapper" class="d-flex flex-column">
+
+      <!-- Main Content -->
+      <div id="content">
+
+        <!-- Topbar -->
+        @include('layouts.partials.mainheader')
+        <!-- End of Topbar -->
+
+        <!-- Begin Page Content -->
+        <div class="container-fluid">
+          @yield('main-content')
+        </div>
+        <!-- /.container-fluid -->
+
+      </div>
+      <!-- End of Main Content -->
+
+      <!-- Footer -->
+      @include('layouts.partials.footer')
+      <!-- End of Footer -->
+
+    </div>
+    <!-- End of Content Wrapper -->
+
+  </div>
+  <!-- End of Page Wrapper -->
+
+  <!-- Scroll to Top Button-->
+  <a class="scroll-to-top rounded" href="#page-top">
+    <i class="fas fa-angle-up"></i>
+  </a>
+
+  <!-- Logout Modal-->
+  @include('layouts.partials.logoutmodal')
+
+
+
+
+  <!-- Script Section -->
+  @include('layouts.partials.script')
+  
+</body>
+
+</html>
